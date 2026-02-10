b00856b6842d70242861da48130013f107018c56
SadhasivamArul <sadhasivamarul8825@gmail.com>
2026-02-10 11:40:30 +0530

    Update and rename DATABASE to DATABASE.sql

---
+++ DATABASE.sql
@@ -0,0 +1,10 @@
+create database studentsDB;
+use studentsDB;
+create table students(
+Reg_number int primary key,
+Student_Name varchar(20),
+Email varchar(30),
+Age int,
+Gender varchar(30),
+Course varchar(30),
+);
